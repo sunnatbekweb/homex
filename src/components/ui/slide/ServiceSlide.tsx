ac806c603ff6bdf0246aacb844be1fb3e3f99f56
sunnatbekweb <sunnatbekkadamov@gmail.com>
2026-03-09 16:41:44 +0500

    fancybox to images

--- src/components/ui/slide/ServiceSlide.tsx
+++ src/components/ui/slide/ServiceSlide.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useGetServices } from '@/hooks/api.query'
+import useFancybox from '@/hooks/useFancybox'
 import { TLocale } from '@/types'
 import { useLocale } from 'next-intl'
-import { useEffect, useState } from 'react'
 import { Autoplay } from 'swiper/modules'
 
 // Import Swiper React components
@@ -13,24 +13,15 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import Image from 'next/image'
 import 'swiper/css'
 
-import { ServiceImageModal } from '../modal/ServiceImageModal'
 import './style.scss'
 
 export const ServiceSlide = () => {
 	const { data } = useGetServices()
 	const locale = useLocale() as TLocale
-	const [activeImage, setActiveImage] = useState<string | null>(null)
-
-	useEffect(() => {
-		if (activeImage && document) {
-			document.body.style.overflowY = 'hidden'
-		} else {
-			document.body.style.overflowY = 'auto'
-		}
-	}, [activeImage])
+	const [fancyboxRef] = useFancybox()
 
 	return (
-		<div>
+		<div ref={fancyboxRef}>
 			<Swiper
 				slidesPerView={1}
 				spaceBetween={15}
@@ -58,29 +49,28 @@ export const ServiceSlide = () => {
 						className="relative rounded-[10px] overflow-hidden"
 						key={index}
 					>
-						<Image
-							src={service?.image}
-							width={300}
-							height={260}
-							alt={service?.[`title_${locale}`]}
-							className="w-full aspect-square object-cover"
-							onClick={() => setActiveImage(service.image)}
-						/>
-						<strong
-							title={service?.[`title_${locale}`]}
-							className="absolute left-5 bottom-5 px-2.5 py-1.25 rounded-md bg-primary text-sm leading-none text-white w-fit"
+						<div
+							data-fancybox="service_images"
+							data-src={service?.image}
 						>
-							{service?.[`title_${locale}`]}
-						</strong>
-						<div className="bg-black/20 w-full h-full absolute top-0 left-0 pointer-events-none"></div>
+							<Image
+								src={service?.image}
+								width={300}
+								height={260}
+								alt={service?.[`title_${locale}`]}
+								className="w-full aspect-square object-cover"
+							/>
+							<strong
+								title={service?.[`title_${locale}`]}
+								className="absolute left-5 bottom-5 px-2.5 py-1.25 rounded-md bg-primary text-sm leading-none text-white w-fit"
+							>
+								{service?.[`title_${locale}`]}
+							</strong>
+							<div className="bg-black/20 w-full h-full absolute top-0 left-0 pointer-events-none"></div>
+						</div>
 					</SwiperSlide>
 				))}
 			</Swiper>
-			<ServiceImageModal
-				image={activeImage || ''}
-				isVisible={!!activeImage}
-				close={() => setActiveImage(null)}
-			/>
 		</div>
 	)
 }
